5640b6239411f125856b2b3e05cc815204efe0b9
Anice Nelly GITHUB <uwasenelly2000@gmail.com>
2023-10-27 02:40:59 -1200

    adding chart and cards to dashboard

--- src/component/charts.js
+++ src/component/charts.js
@@ -1,72 +1,70 @@
 import React from 'react';
-import { Line, Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+import {
+Chart as ChartJS,
+LineElement,
+CategoryScale,
+PointElement,
+LinearScale
+}
+from 'chart.js';
+
+ChartJS.register(
+LineElement,
+CategoryScale,
+LinearScale,
+PointElement
+)
 
 const Charts = () => {
-    const lineChartData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        const data = { 
+        labels: ['Users', 'Blogs', 'Locations', 'Views'],
         datasets: [
           {
-            label: 'Monthly Sales',
-            data: [12, 19, 3, 5, 2, 3],
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 2,
-          },
-        ],
+            data: [100, 75, 50, 25],
+            backgroundColor:'transparent',
+            backColor: '#f2bc8d',
+            pointBorderColor: 'transparent',
+            pointBorderwidth: 4,
+            tension: 0.5 
+          }
+        ]
       };
     
-      const barChartData = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: 'Favorite Colors',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'red',
-              'blue',
-              'yellow',
-              'green',
-              'purple',
-              'orange',
-            ],
-          },
-        ],
-      };
-    
-      const lineChartOptions = {
-        scales: {
-          x: {
-            beginAtZero: true,
-          },
-          y: {
-            beginAtZero: true,
-          },
+      const options = {
+        Plugins:{
+          legend: false
         },
+        Scales:
+        {
+       x: {
+        grid:
+        {
+          display:false
+        }
+       },
+       y: {
+         min: 2,
+         max: 10,
+         ticks:{
+          stepSize: 2,
+          callback: (value) => value + 'K'
+         },
+         grid: {
+         borderDash: [10]
+         }
+       }
+        }
       };
-    
-      const barChartOptions = {
-        scales: {
-          x: {
-            beginAtZero: true,
-          },
-          y: {
-            beginAtZero: true,
-          },
-        },
-      };
-    
+         
       return (
-        <div>
-          <h1>Dashboard</h1>
-          <div className="chart">
-            <h2>Line Chart</h2>
-            <Line data={lineChartData} options={lineChartOptions} />
-          </div>
-          <div className="chart">
-            <h2>Bar Chart</h2>
-            <Bar data={barChartData} options={barChartOptions} />
-          </div>
+        <div style={{width: '100%', height: 'auto'}}>
+        <Line data={data} options={options}></Line>
         </div>
       );
-    };
+    }
     
 export default Charts;
+
+
+
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,9 +3,11 @@ import Dnav from '../component/dash navbar'
 import Footer from '../component/footer'
 import { Link } from 'react-router-dom'
 import InnerDashMenu from '../component/InnerDashMenu'
-import Dashcontent from '../component/dashcontent'
-import ReactDOM from 'react-dom';
 import Charts from '../component/charts'
+import {  FiUsers } from 'react-icons/fi'
+import {  MdOutlinePostAdd } from 'react-icons/md'
+import { LiaSearchLocationSolid } from 'react-icons/lia'
+import { LuView } from 'react-icons/lu'
 
 function Dashboard() {
   
@@ -21,25 +23,41 @@ function Dashboard() {
       <p>Hello Admin,
       this journey of discovery and enlightenment,your Creation of Blog Together with
        managing it should Add Value To The Society. Welcome and enjoy your Time!
-       Hello Admin,
-      this journey of discovery and enlightenment,your Creation of Blog Together with
-       managing it should Add Value To The Society. Welcome and enjoy your Time!
-       Hello Admin,
-      this journey of discovery and enlightenment,your Creation of Blog Together with
-       managing it should Add Value To The Society. Welcome and enjoy your Time!</p>
-       <div className='dashboard-grid-content'>
-       <div className='UUU'><h4>90K+</h4><br /><p>Blogs</p></div>
-       <div className='AAA'><h4>90K+</h4><br /><p>Locations</p></div>
-       <div className='BBB'><h4>90K+</h4><br /><p>Ideas</p></div>
-       </div>
-      </div>
+    </p>
+    </div>
     </section>
-
-
-
-
-    
-   <Footer/>
+    <div className='containing-card'>
+    <div className="card1">
+    <div class="container">
+      <FiUsers className='card-icon'/>
+      <h4><b>100+</b></h4> <br></br>
+      <p>Users</p> 
+    </div>
+    </div>
+    <div className="card2">
+    <div class="container">
+    <MdOutlinePostAdd className='card-icon'/>
+      <h4><b>200+</b></h4> <br></br>
+      <p>Blogs</p> 
+    </div>
+</div>
+  <div className="card3">
+  <div class="container">
+  <LiaSearchLocationSolid className='card-icon'/>
+    <h4><b>300+</b></h4> <br></br>
+    <p>Locations</p> 
+  </div>
+</div>
+  <div class="card4">
+  <div class="container">
+  <LuView className='card-icon'/>
+    <h4><b>400+</b></h4> <br></br>
+    <p>Views</p> 
+  </div>
+</div>
+</div>
+   <Charts/>
+   <Footer className='dashboard-footer'/>
     </div>
   )
 }
